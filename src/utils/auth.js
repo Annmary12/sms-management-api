359cf109d9c27645af24362de4727aaaed74aeaa
Annmary12 <annmaryamaka@yahoo.com>
2019-04-22 18:16:00 +0100

    add jsdoc to generate token function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,10 @@
 import jwt from 'jsonwebtoken';
 
+/**
+ * @description generates token
+ * @param {Object} payload
+ * @returns token
+ */
 export const generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.TOKEN_EXPIRES
