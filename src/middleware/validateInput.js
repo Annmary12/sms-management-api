a84e9bcf2fa20016ef12855330f52bf5c6f67375
Annmary12 <annmaryamaka@yahoo.com>
2019-04-22 16:59:45 +0100

    add contact input validator

---
+++ src/middleware/validateInput.js
@@ -0,0 +1,15 @@
+import ErrorHandler from '../utils/errorHandler';
+import { readSync } from 'fs';
+
+const ValidateInputs = {
+  contactInputValidation: (req, res, next) => {
+    req.checkBody('name', 'name is required').trim().notEmpty();
+    req.checkBody('phoneNumber', 'phone number is required').trim().notEmpty();
+    req.checkBody('phoneNumber', 'phone number must be an integer').isNumeric();
+    req.checkBody('phoneNumber', 'phone nunmber must not exceed 12 digits').isLength({ min: 12, max: 12 });
+
+    ErrorHandler(req, res, next)
+  }
+}
+
+export default ValidateInputs;
