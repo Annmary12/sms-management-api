ff32bff0abe9b4562abe129bc3cd293f8c416856
Annmary12 <annmaryamaka@yahoo.com>
2019-04-22 18:02:43 +0100

    add jsdoc to create function in baserepository

--- src/repository/BaseRepository.js
+++ src/repository/BaseRepository.js
@@ -1,4 +1,14 @@
+/**
+ * @description Base repository
+ * @class BaseRepository
+ */
 class BaseRepository {
+  /**
+   * @description creates a new document
+   * @param {Object} Model
+   * @param {Object} options
+   * @returns {Document} returns a newly created document
+   */
   static async create(Model, options) {
     try {
       return await Model.create(options);
