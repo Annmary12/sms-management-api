07ff9faf2e77bc2285c6a189c1bb17e03ce9f4a1
Annmary12 <annmaryamaka@yahoo.com>
2019-04-22 18:13:11 +0100

    add jsdoc to create function in contact controller

--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -2,7 +2,17 @@ import Contact from '../models/contact';
 import BaseRepository from '../repository/BaseRepository';
 import { generateToken } from '../utils/auth';
 
+/**
+ * @description ContactController
+ * @class ContactController
+ */
 class ContactController {
+  /**
+   * @description creates new contact
+   * @param {Object} req request object
+   * @param {Object} res response object
+   * @returns {json} status code, message and newly created contact
+   */
   static async create(req, res) {
     try {
       const { name, phoneNumber } = req.body;
