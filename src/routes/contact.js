b48e6df227c9a346f31edaa8b3c58cfe69bd53a3
Annmary12 <annmaryamaka@yahoo.com>
2019-04-28 15:52:38 +0100

    add signin route

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -5,13 +5,13 @@ import checkContactExist from '../middleware/checkContact';
 import { hasToken, verifyToken } from '../middleware/auth';
 
 // destructuring the methods
-const { create, getAll, getOne, deleteUser } = ContactController;
+const { create, getAll, getOne, deleteUser, signIn } = ContactController;
 const { contactInputValidation } = ValidateInputs;
 
 const router = Router();
 
 router.post('/', contactInputValidation, checkContactExist, create);
-
+router.post('/sign-in', signIn);
 
 // add middleware to verify token
 router.use('/', hasToken, verifyToken);
